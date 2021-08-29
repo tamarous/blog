71f4b4017eade18d0b071778ccc992153116add9
Tamarous <hiwangzewei@qq.com>
2021-08-29 15:20:55 +0800

    配置博客主页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,5 +13,12 @@ module.exports = config({
     docsDir: "docs",
     docsBranch: "master",
     sidebar: "auto",
+    blog: {
+      links: {
+        'Github': 'https://github.com/tamarous',
+        'Twitter': 'https://twitter.com/tamarous',
+        'Weibo': 'https://weibo.com/u/2200104011'
+      }
+    }
   },
 });
--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,6 @@
 ---
 home: true
+blog: true
 heroText: Tamarous's blog
 tagline: 记录 Tamarous 学习工作中的一些笔记
 actionText: 了解更多 →
