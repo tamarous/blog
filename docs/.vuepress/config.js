c120f50fc3f5a075f8eb1b4250b190cc145415cf
wangzewei <hiwangzewei@qq.com>
2024-02-25 12:22:59 +0800

    修复链接错误

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = config({
     nav: [
       { text: "Home", link: "/" },
       { text: "iOS", link: "/iOS/" },
-      { text: "Backend", link: "/Backend"}
+      { text: "Backend", link: "/Backend/"}
     ],
     repo: "tamarous/blog",
     repoLabel: "GitHub",
